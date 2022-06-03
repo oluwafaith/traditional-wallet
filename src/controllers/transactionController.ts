f619cc813295caaf888d16d15dee0fba4d5b8b46
faith okpala <okpalafaith1@gmail.com>
2022-06-03 15:34:52 +0100

    worked on transactions

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -1,8 +1,10 @@
 import {Request, Response, NextFunction } from 'express'
 import {User} from "../models/userModel"
 import {Transaction} from "../models/transactionModel"
+import {Product} from "../models/productModel"
 import CustomError from "../errors"
 import { catchAsync } from "../utils/catchAsync"
+import { Order } from '../models/purchaseModel'
 
 
 export const creditAccount = catchAsync(async(req: Request, res: Response, next: NextFunction)=>{
@@ -29,6 +31,7 @@ export const creditAccount = catchAsync(async(req: Request, res: Response, next:
 
 export const debitAccount = catchAsync(async(req: Request, res: Response, next: NextFunction)=>{
     const {email, amount} = req.body;
+
     const user = await User.findOne({ email });
     
     if (!user) {
@@ -40,6 +43,36 @@ export const debitAccount = catchAsync(async(req: Request, res: Response, next: 
       }
 
   const updatedWallet = await User.findOneAndUpdate({email}, { $inc: { wallet: amount } });
+  
+  const transaction = await Transaction.create([{
+    amount,
+    name: user.name,
+    email: user.email,
+    balanceBefore: user.wallet,
+    balanceAfter: user.wallet - Number(amount),
+  }]);
+  res.status(201).json({ 
+    message: 'Withdrawal successful',
+      data:  updatedWallet, transaction });
+})
+
+export const purchaseProduct = catchAsync(async(req: Request, res: Response, next: NextFunction)=>{
+  const {amount} = req.body
+  const { id: orderId } = req.params;
+  const order = await Order.findOne({ _id: orderId });
+
+
+    const user = await User.findOne({ orderId });
+    
+    if (!user) {
+        throw new CustomError.UnauthenticatedError('Invalid Credentials');
+      }
+     
+      if(user.wallet < amount){
+        throw new CustomError.BadRequestError('Insufficient balance');
+      }
+
+  const updatedWallet = await User.findOneAndUpdate({orderId}, { $inc: { wallet: amount } });
   
   const transaction = await Transaction.create([{
     amount,
--- src/routes/transactionRoute.ts
+++ src/routes/transactionRoute.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 const router = express.Router();
 import {creditAccount, debitAccount, walletBalance} from '../controllers/transactionController'
+import {
+    authenticateUser,
+    authorizePermissions,
+  } from '../middleWare/authentication';
 
 router.post('/deposit', creditAccount);
-router.post('/withdraw', debitAccount);
-router.get('/balance', walletBalance);
+router.post('/withdraw', authenticateUser, debitAccount);
+router.get('/balance',authenticateUser, walletBalance);
 
 
 export default router;
