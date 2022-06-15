3f81ef5c9f0cd369bf6ec45245fcdefa00cef6d2
faith okpala <okpalafaith1@gmail.com>
2022-06-15 15:31:10 +0100

    removed console.log

--- src/utils/checkPermission.ts
+++ src/utils/checkPermission.ts
@@ -1,10 +1,10 @@
 import CustomError from "../errors/index";
 
 const checkPermissions = (requestUser: any, resourceUserId: any) => {
-  console.log("kiuytgh ");
+ 
   
   if (requestUser.role === "admin") return;
-  console.log(requestUser, "kil");
+ 
   
   if (requestUser.userId === resourceUserId.toString()) return;
   throw new CustomError.UnauthorizedError(
